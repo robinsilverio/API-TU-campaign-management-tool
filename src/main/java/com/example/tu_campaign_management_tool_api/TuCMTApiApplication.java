d1f3d838085d4f7d35db93b7de85eb53b2f2b6ed
Robin Medeiros Silvério <r.medeiros.silverio@technischeunie.com>
2023-06-28 19:57:28 +0200

    Initial commit. Set up SpringBoot project.

---
+++ src/main/java/com/example/tu_campaign_management_tool_api/TuCMTApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.tu_campaign_management_tool_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuCMTApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuCMTApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tu_campaign_management_tool_api/TuCMTApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tu_campaign_management_tool_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuCMTApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
