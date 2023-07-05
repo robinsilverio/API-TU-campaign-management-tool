cb155337050f6c7185897a9417f85a72cb19b81f
Robin Medeiros Silvério <r.medeiros.silverio@technischeunie.com>
2023-07-05 15:13:27 +0200

    Added a test suite for running tests in packages.

--- src/test/java/com/example/tu_campaign_management_tool_api/TuCMTApiApplicationTests.java
+++ src/test/java/com/example/tu_campaign_management_tool_api/TuCMTApiApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.tu_campaign_management_tool_api;
 
 import org.junit.jupiter.api.Test;
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SelectPackages({
+        "com.example.tu_campaign_management_tool_api.authServices",
+        "com.example.tu_campaign_management_tool_api.controllers",
+        "com.example.tu_campaign_management_tool_api.jwtUtils"
+})
+@Suite
 class TuCMTApiApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
