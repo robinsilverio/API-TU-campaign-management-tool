1b408e01ffc9ae9e88ec7c50e4235d8d106df425
Robin Medeiros Silvério <r.medeiros.silverio@technischeunie.com>
2023-07-06 12:08:25 +0200

    Added a dependency for permitting running tests at once using suite.

--- src/test/java/com/example/tu_campaign_management_tool_api/TuCMTApiApplicationTests.java
+++ src/test/java/com/example/tu_campaign_management_tool_api/TuCMTApiApplicationTests.java
@@ -1,17 +1,14 @@
 package com.example.tu_campaign_management_tool_api;
 
-import org.junit.jupiter.api.Test;
-import org.junit.platform.suite.api.IncludeTags;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
-import org.springframework.boot.test.context.SpringBootTest;
 
+@Suite
 @SelectPackages({
         "com.example.tu_campaign_management_tool_api.authServices",
         "com.example.tu_campaign_management_tool_api.controllers",
         "com.example.tu_campaign_management_tool_api.jwtUtils"
 })
-@Suite
 class TuCMTApiApplicationTests {
 
 }
