362ca16e4f4052504b8652bcfe5a34a4e9e0ff93
Robin Medeiros Silvério <r.medeiros.silverio@technischeunie.com>
2023-07-08 23:08:20 +0200

    Refactored code and added more tests like checking if token is not empty.

--- src/test/java/com/example/tu_campaign_management_tool_api/jwtUtils/TestJwtUtils.java
+++ src/test/java/com/example/tu_campaign_management_tool_api/jwtUtils/TestJwtUtils.java
@@ -1,6 +1,8 @@
 package com.example.tu_campaign_management_tool_api.jwtUtils;
 
 import com.example.tu_campaign_management_tool_api.security.services.UserDetailsImpl;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,6 +11,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.Authentication;
 
 import java.util.Arrays;
+import java.util.Date;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -24,31 +27,38 @@ public class TestJwtUtils {
 
     private JwtUtilsMock jwtUtils;
 
-    private String jwtSecret;
-    private int jwtExpirationMs; // 1 hour
+    private String jwtSecret = "ZxGy+zQ7DImWbxguAPFFku47wRtzRnHy29bFFCKdKPg=";
     private UserDetailsImpl userDetailsImpl;
     private String jwtToken;
     private boolean expectedBoolean;
+    private boolean actualBoolean;
 
-    public void arrangeJwtUtilsMock() {
+    private void arrangeJwtUtilsMock() {
         this.jwtUtils = new JwtUtilsMock(
-                "ZxGy+zQ7DImWbxguAPFFku47wRtzRnHy29bFFCKdKPg=", 3600000);
+                this.jwtSecret, 3600000);
     }
 
-    public void arrangeUserDetailsImpl() {
-        userDetailsImpl = new UserDetailsImpl(
+    private void arrangeUserDetailsImpl() {
+        this.userDetailsImpl = new UserDetailsImpl(
                 "1", "Robin",
                 "test123", Arrays.asList(() -> "Super User e-Sales"));
     }
 
-    public void arrangeBooleanAndJwtToken(boolean paramBoolean, String paramJwtToken) {
-        expectedBoolean = paramBoolean;
+    private void arrangeExpectedBoolean(boolean paramBoolean) {
+        this.expectedBoolean = paramBoolean;
+    }
+
+    private void arrangeActualBoolean(String paramJwtToken) {
+        this.actualBoolean = this.jwtUtils.validateJwtToken(paramJwtToken);
+    }
+
+    private void arrangeJwtToken(String paramJwtToken) {
         this.jwtToken = paramJwtToken;
     }
 
     private void actGenerateToken() {
         when(this.authentication.getPrincipal()).thenReturn(this.userDetailsImpl);
-        jwtToken = this.jwtUtils.generateJwtToken(this.authentication);
+        this.jwtToken = this.jwtUtils.generateJwtToken(this.authentication);
     }
 
     @Test
@@ -59,7 +69,6 @@ public class TestJwtUtils {
 
         // Act.
         actGenerateToken();
-
         // Assert.
         Assertions.assertNotNull(this.jwtToken);
     }
@@ -73,7 +82,6 @@ public class TestJwtUtils {
         // Act.
         actGenerateToken();
         String tokenSubject = this.jwtUtils.getUserNameFromJwtToken(this.jwtToken);
-
         // Assert
         assertEquals(expectedUsername, tokenSubject);
     }
@@ -81,11 +89,34 @@ public class TestJwtUtils {
     @Test
     public void should_return_false_when_jwtToken_is_invalid() {
         // Arrange
-        arrangeBooleanAndJwtToken(false, "aaa");
+        arrangeJwtToken("aaa");
         arrangeJwtUtilsMock();
         // Act
-        boolean actualBoolean = this.jwtUtils.validateJwtToken(this.jwtToken);
+        arrangeActualBoolean(this.jwtToken);
         // Assert
-        assertThat(actualBoolean, is(expectedBoolean));
+        assertThat(actualBoolean, is(this.expectedBoolean));
+    }
+
+    @Test
+    public void should_return_false_when_jwtToken_is_expired() {
+        // Generate an expired JWT token
+        arrangeExpectedBoolean(false);
+        Date expirationDate = new Date(System.currentTimeMillis() - 1000);
+        arrangeJwtUtilsMock();
+        String expiredToken = Jwts.builder()
+                .setExpiration(expirationDate)
+                .signWith(SignatureAlgorithm.HS256, this.jwtSecret)
+                .compact();
+        arrangeActualBoolean(expiredToken);
+        assertThat(this.actualBoolean, is(this.expectedBoolean));
+    }
+    
+    @Test
+    public void should_return_false_when_jwtToken_is_empty() {
+        boolean expectedBoolean = false;
+        arrangeJwtUtilsMock();
+        String emptyToken = "";
+        arrangeActualBoolean(emptyToken);
+        assertThat(this.actualBoolean, is(expectedBoolean));
     }
 }
