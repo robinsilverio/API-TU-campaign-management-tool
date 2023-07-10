2c662cf1b37d0695605010d680f576f5c66e30f3
Robin Medeiros Silvério <r.medeiros.silverio@technischeunie.com>
2023-07-10 14:26:15 +0200

    Permit using resource url's that are related to campaigns.

--- src/main/java/com/example/tu_campaign_management_tool_api/security/WebSecurityConfiguration.java
+++ src/main/java/com/example/tu_campaign_management_tool_api/security/WebSecurityConfiguration.java
@@ -75,7 +75,7 @@ public class WebSecurityConfiguration {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("/api/auth/**").permitAll()
-                                .requestMatchers("/api/test/**").permitAll()
+                                .requestMatchers("/api/campaign/**").permitAll()
                                 .anyRequest().authenticated()
                 );
 
