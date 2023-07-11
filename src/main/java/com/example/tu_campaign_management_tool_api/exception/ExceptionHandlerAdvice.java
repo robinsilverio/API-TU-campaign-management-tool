ba2412474ed6dcf943e7d4a9aef16b59609414bd
Robin Medeiros Silvério <r.medeiros.silverio@technischeunie.com>
2023-07-11 14:55:45 +0200

    Added an exception in case that Spring cannot write objects to JSON.

--- src/main/java/com/example/tu_campaign_management_tool_api/exception/ExceptionHandlerAdvice.java
+++ src/main/java/com/example/tu_campaign_management_tool_api/exception/ExceptionHandlerAdvice.java
@@ -2,6 +2,7 @@ package com.example.tu_campaign_management_tool_api.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -19,7 +20,12 @@ public class ExceptionHandlerAdvice {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException e) {
-        // Handle the MethodArgumentNotValidException here
         return ResponseEntity.badRequest().body("Invalid request body: " + e.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotWritableException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<String> handleWritableException(HttpMessageNotWritableException e) {
+        return ResponseEntity.internalServerError().body("Error " + HttpStatus.INTERNAL_SERVER_ERROR + ": " + e.getMessage());
     }
 }
