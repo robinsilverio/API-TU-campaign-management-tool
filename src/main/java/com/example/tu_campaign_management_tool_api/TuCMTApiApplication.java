7c63cf68c3c806b9fbfdbd5b26ab41cf5b479ca0
Robin Medeiros Silvério <r.medeiros.silverio@technischeunie.com>
2023-10-04 13:40:45 +0200

    Updated application.properties. In some files, debug mode is removed. Also, all files are provided with encrypted passwords for establishing database connections.

--- src/main/java/com/example/tu_campaign_management_tool_api/TuCMTApiApplication.java
+++ src/main/java/com/example/tu_campaign_management_tool_api/TuCMTApiApplication.java
@@ -1,9 +1,11 @@
 package com.example.tu_campaign_management_tool_api;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class TuCMTApiApplication {
 
     public static void main(String[] args) {
