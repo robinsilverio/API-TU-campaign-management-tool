80849d4645b49bccfa370c10f4250a3f55e1ef94
robinsilverio <robinsilverio17@gmail.com>
2024-09-27 15:35:32 +0200

    Update configuration file.

--- src/main/java/com/example/tu_campaign_management_tool_api/TuCMTApiApplication.java
+++ src/main/java/com/example/tu_campaign_management_tool_api/TuCMTApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableEncryptableProperties
 public class TuCMTApiApplication {
 
     public static void main(String[] args) {
