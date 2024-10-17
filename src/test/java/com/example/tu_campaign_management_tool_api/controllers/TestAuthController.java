74b79bf524c0a9b99d540b8f2dae4c61b39a4483
robinsilverio <robinsilverio17@gmail.com>
2024-10-17 12:16:59 +0200

    Wrote test case for validating jwt token.

--- src/test/java/com/example/tu_campaign_management_tool_api/controllers/TestAuthController.java
+++ src/test/java/com/example/tu_campaign_management_tool_api/controllers/TestAuthController.java
@@ -2,6 +2,8 @@ package com.example.tu_campaign_management_tool_api.controllers;
 
 import com.example.tu_campaign_management_tool_api.jwtUtils.JwtUtilsMock;
 import com.example.tu_campaign_management_tool_api.payload.request.LoginRequest;
+import com.example.tu_campaign_management_tool_api.payload.responses.ErrorResponse;
+import com.example.tu_campaign_management_tool_api.payload.responses.RolesResponse;
 import com.example.tu_campaign_management_tool_api.repositories.UserRepository;
 import com.example.tu_campaign_management_tool_api.security.services.UserDetailsImpl;
 import org.junit.jupiter.api.Test;
@@ -10,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -20,7 +23,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -51,6 +57,10 @@ public class TestAuthController {
     private UserDetailsImpl userDetails;
     @Mock
     public JwtUtilsMock jwtUtils;
+
+    private void mockAuthControllerMvc() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(authController).build();
+    }
 
     @Test
     public void should_return_a_statusCode200_containing_an_object_with_token_and_userDetails_when_authentication_went_successful() {
@@ -89,8 +99,38 @@ public class TestAuthController {
     }
 
     @Test
+    public void should_return_role_of_eSales_when_jwtToken_is_valid() {
+        // Arrange.
+        String jwtToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJUMTQ4MzIiLCJyb2xlcyI6WyJST0xFX1NVUEVSX1VTRVJfRS1TQUxFUyJdLCJpYXQiOjE3MjkxNTQwNzYsImV4cCI6MTcyOTI3NzUzM30.fCtZ-KEax90Vv6U478esk06jAGbVnGiPgcepedikMZY";
+        String expectedRole = "ROLE_SUPER_USER_E-SALES";
+
+        // Act.
+        when(jwtUtils.validateJwtToken(jwtToken)).thenReturn(true);
+        when(jwtUtils.getRolesFromJwtToken(jwtToken)).thenReturn(List.of("ROLE_SUPER_USER_E-SALES"));
+        ResponseEntity<RolesResponse> response = (ResponseEntity<RolesResponse>) authController.validateJwtToken(jwtToken);
+
+        // Assert.
+        assertThat(response.getBody().getRoles().get(0), is(expectedRole));
+
+    }
+
+    @Test
+    public void should_return_errorMessage_when_jwtToken_is_invalid() {
+        // Arrange.
+        String expectedErrorMessage = "Invalid Token";
+
+        // Act.
+        when(jwtUtils.validateJwtToken(null)).thenReturn(false);
+        ResponseEntity<ErrorResponse> response = (ResponseEntity<ErrorResponse>) authController.validateJwtToken(null);
+
+        // Arrange.
+        assertThat(expectedErrorMessage, is(response.getBody().getErrorMessage()));
+    }
+
+    @Test
     public void should_return_statusCodeOf400_when_loginRequest_is_invalid() throws Exception {
-        mockMvc = MockMvcBuilders.standaloneSetup(authController).build();
+
+        mockAuthControllerMvc();
 
         String requestJson = "{\"login\":\"robin\", \"password\":\"test123\"}";
 
@@ -99,6 +139,22 @@ public class TestAuthController {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(requestJson))
                 .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    public void should_return_statusCodeOf401_when_jwtToken_is_invalid() throws Exception {
+        mockAuthControllerMvc();
+        String requestParam = "";
+        LinkedMultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+        requestParams.add("paramToken", requestParam);
+
+        // Act
+        when(jwtUtils.validateJwtToken(requestParam)).thenReturn(false);
+
+        // Assert
+        mockMvc.perform(MockMvcRequestBuilders.get("/auth/validateJwt")
+                .params(requestParams))
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
     }
 
 }
